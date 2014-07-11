74e99c6215202880fa7e5abf336e7cabcdb30919
Mark Lavin <mlavin@caktusgroup.com>
2014-07-11 08:49:15 -0400

    Add playback button rather than auto-playback.

--- js/webapp.js
+++ js/webapp.js
@@ -2,20 +2,26 @@
 "use strict";
 (function () {
     var toggleButton = document.querySelector("#record-toggle"),
+        playbackButton = document.querySelector("#playback"),
         recording = false,
         recorder = new Recorder();
 
     toggleButton.onclick = function () {
         if (!recording) {
             toggleButton.innerHTML = 'Stop Recording';
+            playbackButton.style.display = "none";
             recording = true;
             recorder.start();
         } else {
             toggleButton.innerHTML = 'Start Recording';
+            playbackButton.style.display = "block";
             recording = false;
             recorder.stop();
-            recorder.play();
         }
+    };
+
+    playbackButton.onclick = function () {
+        recorder.play();
     };
 
 })();
